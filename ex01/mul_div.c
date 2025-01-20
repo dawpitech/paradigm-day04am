592b416d0c7b19984392365762ea5ec598de7e28
Antoine <me@antoinebellanger.fr>
2025-01-20 11:02:51 +0100

    fix: ex01 pointer check

--- ex01/mul_div.c
+++ ex01/mul_div.c
@@ -5,8 +5,12 @@
 ** main C file
 */
 
+#include <stddef.h>
+
 void mul_div_long(int a, int b, int *mul, int *div)
 {
+    if (mul == NULL || div == NULL)
+        return;
     *mul = a*b;
     if (b == 0)
         *div = 42;
@@ -16,6 +20,8 @@ void mul_div_long(int a, int b, int *mul, int *div)
 
 void mul_div_short(int *a, int *b)
 {
+    if (a == NULL || b == NULL)
+        return;
     *a = (*a)*(*b);
     if (*b == 0)
         *b = 42;
